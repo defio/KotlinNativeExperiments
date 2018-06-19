b4754433d5b97c931f38cf9e23c362d27333543e
Nicola De Fiorenze <n.defiorenze@doveconviene.com>
2018-06-19 23:13:56 +0200

    finally done

--- androidApp/app/src/test/java/com/ndefiorenze/samplemultiplatformkotlinapp/ExampleUnitTest.kt
+++ androidApp/app/src/test/java/com/ndefiorenze/samplemultiplatformkotlinapp/ExampleUnitTest.kt
@@ -11,8 +11,9 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
+    fun sampleTest() {
         assertEquals("Hello, Android", Greeting().greeting())
     }
 }
